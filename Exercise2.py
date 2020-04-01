8bee22cab530de1996d2684ebad8e56bb649355d
Kelsey Cortez <kelscortez@gmail.com>
2020-04-01 19:20:44 -0400

    finished

--- Exercise2.py
+++ Exercise2.py
@@ -5,3 +5,6 @@ name = input('What is your name? '.upper())
 
 greeting = f'Hello, {name}! '
 print (greeting.upper())
+
+letters_in_name = f'Your name has {len(name)} letters'
+print(letters_in_name) 
