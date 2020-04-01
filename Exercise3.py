9d84dbcedadfdb9a550af6cd5e32253cb28c6283
Kelsey Cortez <kelscortez@gmail.com>
2020-04-01 19:57:25 -0400

    finished

--- Exercise3.py
+++ Exercise3.py
@@ -1,4 +1,18 @@
 # madlib using input function
 
-print ('Please fill in the blanks below: ')
+name = ''
+color = ''
+result = ''
 
+start_lib = ('Please fill in the blanks below: ')
+my_lib = "___(name)___'s favorite color is  ___(color)___"
+
+print(start_lib)
+print(my_lib)
+
+name = input('What is your name? ')
+color = input('What is your favorite color? ')
+
+result = f"{name}'s favorite color is {color}."
+
+print(result)
